c9f93009804fa4efad462d76c87dfc5ecd7dde8b
Martin Schmitz <Martin@familieschmitz.de>
2023-07-05 14:28:31 +0200

    on bush
    ~

--- src/app/change-detection/on-push-async/on-push-async.component.ts
+++ src/app/change-detection/on-push-async/on-push-async.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { StoreService } from '../store.service';
+import { ToDoItem } from '../model';
 
 @Component({
   selector: 'app-on-push-async',
   templateUrl: './on-push-async.component.html',
-  styleUrls: ['./on-push-async.component.scss']
+  styleUrls: ['./on-push-async.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OnPushAsyncComponent {
+  items: ToDoItem[] = [];
 
+  constructor(private readonly todoStore: StoreService) {
+    todoStore.getTodos$().subscribe((todos) => {
+      this.items = todos.todos;
+    });
+  }
+
+  public addTodo(value: string) {
+    this.todoStore.addTodo({ title: value });
+    this.items = this.todoStore.getTodos();
+  }
+
+  public todoCompleted(item: ToDoItem) {
+    this.todoStore.editTodo(item.id, (draft) => {
+      draft.completed = !draft.completed;
+    });
+    this.items = this.todoStore.getTodos();
+  }
 }
