978ff7a78d3fbc632e8576a8d09b54951b6793a3
Martin Schmitz <Martin@familieschmitz.de>
2023-07-05 14:15:42 +0200

    default gedöns

--- src/app/change-detection/default/default.component.ts
+++ src/app/change-detection/default/default.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { StoreService } from '../store.service';
+import { ToDoItem } from '../model';
 
 @Component({
   selector: 'app-default',
   templateUrl: './default.component.html',
-  styleUrls: ['./default.component.scss']
+  styleUrls: ['./default.component.scss'],
 })
 export class DefaultComponent {
+  public items: ToDoItem[] = [];
 
+  constructor(private readonly todoStore: StoreService) {}
+
+  ngOnInit() {
+    this.items = this.todoStore.getTodos();
+  }
+
+  public addTodo(value: string) {
+    this.todoStore.addTodo({ title: value });
+    this.items = this.todoStore.getTodos();
+  }
+
+  public todoCompleted(item: ToDoItem) {
+    this.todoStore.editTodo(item.id, (draft) => {
+      draft.completed = !draft.completed;
+    });
+    this.items = this.todoStore.getTodos();
+  }
 }
